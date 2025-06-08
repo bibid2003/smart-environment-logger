683f6ec99d1e35ceb84143fda2c6845ddf6182b9
bibid2003 <bibidbiju0@gmail.com>
2025-06-08 18:37:47 +0530

    Update alert_manager.py

--- utils/alert_manager.py
+++ utils/alert_manager.py
@@ -2,11 +2,11 @@ import smtplib
 from email.message import EmailMessage
 import time
 
-# --- SETTINGS (replace with yours) ---
+
 SENDER_EMAIL = "bibidbiju0@gmail.com"
 APP_PASSWORD = "zrkv hydc jvce ajqx"
-RECEIVER_EMAIL = "bibidbiju0@gmail.com"  # or any other receiver
-COOLDOWN_SECONDS = 300  # 5 minutes cooldown
+RECEIVER_EMAIL = "bibidbiju0@gmail.com"  
+COOLDOWN_SECONDS = 300  
 
 last_alert_time = 0
 
@@ -14,9 +14,9 @@ def send_email_alert(subject, body):
     global last_alert_time
     current_time = time.time()
 
-    # Cooldown check
+    
     if current_time - last_alert_time < COOLDOWN_SECONDS:
-        print("⚠️ Cooldown active. No alert sent.")
+        print("⚠ Cooldown active. No alert sent.")
         return
 
     msg = EmailMessage()
@@ -29,8 +29,8 @@ def send_email_alert(subject, body):
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
             smtp.login(SENDER_EMAIL, APP_PASSWORD)
             smtp.send_message(msg)
-        print("✅ Email alert sent!")
+        print(" Email alert sent!")
         last_alert_time = current_time
 
     except Exception as e:
-        print("❌ Failed to send email:", e)
+        print(" Failed to send email:", e)
